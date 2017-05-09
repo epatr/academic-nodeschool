eecea3d044df0f653c62babeb0a0366cb2fd8989
Eric Patrick <erstpa@gmail.com>
2017-05-09 01:24:16 -0400

    Baby steps

---
+++ core/02-learnyounode/02-program.js
@@ -0,0 +1,9 @@
+var arguments = process.argv;
+var sum = 0;
+
+for (var i = 2, len = arguments.length; i < len; i++) {
+    // convert to integer
+    sum += Number(arguments[i]);
+}
+
+console.log(sum);
